a324d417d560e75db15b3d8a454cd63923008cf9
github username <github email address>
2020-12-06 10:08:29 -0700

    updates routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(username: "rachel2020", password: "password")
+user2 = User.create(username: "kevin2020", password: "password")
+
+post = Post.create(id: 1, user_id: 1, title: "TITLE", content: "This is my post. It's pretty cool.", image_url: "https://images.unsplash.com/photo-1607242792481-37f27e1d74e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")
+
+comment = Comment.create(post_id: 1, user_id: 2, content: "gorgeous!")
