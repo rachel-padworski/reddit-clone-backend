7bffdcb3c95f7c3595038eea66efc08f320566dd
github username <github email address>
2020-12-06 15:03:46 -0700

    adds foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_163644) do
+ActiveRecord::Schema.define(version: 2020_12_06_215920) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -24,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_163644) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 user = User.create(username: "rachel2020", password: "password")
 user2 = User.create(username: "kevin2020", password: "password")
 
-post = Post.create(id: 1, user_id: 1, title: "TITLE", content: "This is my post. It's pretty cool.", image_url: "https://images.unsplash.com/photo-1607242792481-37f27e1d74e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")
+post = Post.create(user_id: 1, title: "TITLE", content: "This is my post. It's pretty cool.", image_url: "https://images.unsplash.com/photo-1607242792481-37f27e1d74e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")
 
 comment = Comment.create(post_id: 1, user_id: 2, content: "gorgeous!")
